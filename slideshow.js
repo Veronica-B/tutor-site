937a4565525e4775c0b9f9b7349c290d612738fc
Veronica Barcia <veronicabarcia4@gmail.com>
2020-05-05 16:36:45 -0400

    added dots at the bottom of the slideshow

--- slideshow.js
+++ slideshow.js
@@ -1,12 +1,18 @@
-var left_ArrowId = document.getElementById('left-arrow');
-var right_ArrowId = document.getElementById('right-arrow');
-var img=document.getElementById('img');
-var i= 0;
+let left_ArrowId = document.getElementById('left-arrow');
+let right_ArrowId = document.getElementById('right-arrow');
+let img=document.getElementById('img');
+let dot1=document.getElementById('dot1');
+let dot2=document.getElementById('dot2');
+let dot3=document.getElementById('dot3');
+let i= 0;
 
 //0 1 2
-var images= ['images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg'];
+
+const images= ['images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg'];
+const dots=[dot1, dot2, dot3];
+console.log(dots)
 //10000 is equaled to 10 seconds.
-var time= 10000;
+const time= 10000;
 
 
 
@@ -16,15 +22,27 @@ function change_Image(){
 
     switch(i){
         case 0:
+        dots[i].style["height"]="40px"
+        dots[i].style["width"]="40px";
+        dots[2].style["height"]="20px"
+        dots[2].style["width"]="20px";
         i=i+1;
         break;
 
         case 1:
+        dots[i].style["height"]="40px"
+        dots[i].style["width"]="40px";
+        dots[i-1].style["height"]="20px"
+        dots[i-1].style["width"]="20px";
         i=i+1;
         break;
 
         case 2:
-        i=2;
+        dots[i].style["height"]="40px"
+        dots[i].style["width"]="40px";
+        dots[i-1].style["height"]="20px"
+        dots[i-1].style["width"]="20px";    
+        i=0;
         break;    
     }
 }
@@ -34,14 +52,27 @@ function change_Image_Reverse(){
     img.src =images[i];
         switch(i){
             case 0:
-            i=0;
+            dots[i].style["height"]="40px"
+            dots[i].style["width"]="40px";
+            dots[i+1].style["height"]="20px"
+            dots[i+1].style["width"]="20px";    
+            i=2;
             break;
 
             case 1:
+            dots[i].style["height"]="40px"
+            dots[i].style["width"]="40px";
+            dots[i+1].style["height"]="20px"
+            dots[i+1].style["width"]="20px";    
             i=i-1;
+           
             break;
  
             case 2:
+            dots[i].style["height"]="40px"
+            dots[i].style["width"]="40px";
+            dots[0].style["height"]="20px"
+            dots[0].style["width"]="20px";    
             i=i-1;
             break;    
         }
